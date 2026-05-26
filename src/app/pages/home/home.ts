39d26094dedb6e3fe12602f476891aa77b9dbf49
José González <josegf2004@gmail.com>
2026-05-26 02:20:38 -0600

    Cambio total de menu home

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,39 +1,24 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, OnInit, inject } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { TratamientosService } from '../../services/tratamientos';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [RouterModule],
   templateUrl: './home.html',
-  styleUrl: './home.css',
+  styleUrl: './home.css'
 })
-export class Home implements OnInit, OnDestroy {
+export class Home implements OnInit {
 
-  private carruselInterval: any;
-  
+  private router = inject(Router);
+  public tratamientosService = inject(TratamientosService);
+
   ngOnInit(): void {
-    this.iniciarCarrusel();
+    this.tratamientosService.fetchTratamientos();
   }
 
-  ngOnDestroy(): void {
-    if (this.carruselInterval) {
-      clearInterval(this.carruselInterval);
-    }
-  }
-
-  iniciarCarrusel(): void {
-    // Apuntamos al contenedor interno flexible
-    const carruselInner = document.getElementById('CarruselInner');
-    if (carruselInner) {
-      let currentIndex = 0;
-      const totalSlides = carruselInner.children.length;
-
-      this.carruselInterval = setInterval(() => {
-        currentIndex = (currentIndex + 1) % totalSlides;
-        // Desplaza el contenedor horizontalmente basándose en el índice actual (0%, -100%, -200%)
-        carruselInner.style.transform = `translateX(-${currentIndex * 100}%)`;
-      }, 3800); // 3.8 segundos para dar una lectura calmada a los textos
-    }
+  verTratamiento(id: string): void {
+    this.router.navigate(['/tratamiento', id]);
   }
 }
